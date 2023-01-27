6552b7c81d6e5736bd276ca95119dd44f28d3308
Gether1996 <pat.kredatus@gmail.com>
2023-01-27 11:39:00 +0100

    updated config.py, created arena_names.txt and implemented random creating of arena names, based on choices from arena_names.txt

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ WorldMaxDays = int(config['WORLD']["World_Max_Days"])
 NumOfArenaSchedules = int(config['ARENA']["Arena_Schedules"])
 
 arena_names = open('arena_names.txt')
-ListOfArenaNames = lines = [line.rstrip() for line in arena_names]
+ListOfArenaNames = [line.rstrip() for line in arena_names]
 
 
