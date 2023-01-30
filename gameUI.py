b23c0e643068d4e0c3f2ce39b24ca0595c9322a4
Gether1996 <pat.kredatus@gmail.com>
2023-01-30 21:05:58 +0100

    created gameUI with some functions

--- gameUI.py
+++ gameUI.py
@@ -1,31 +1,45 @@
+import os
+from worldGenerator import *
+
+
 class RunGame:
 
-    def __init__(self):
-        pass
+    def __init__(self, world=...):
+        self.world = ...
 
+    def print_header(self):
+        print("ßßßßßßßßßßßßßßßßßßßßßßßß\n"
+              "   Medieval Simulator \n"
+              "ßßßßßßßßßßßßßßßßßßßßßßßß\n")
 
-def start_game():
-    user_input = input("ßßßßßßßßßßßßßßßßßßßßßßßßßß\n"
-                       "    Medieval Simulator \n"
-                       "ßßßßßßßßßßßßßßßßßßßßßßßßßß\n"
-                       "\n"
-                       "1. Generate new world\n"
-                       "2. Load world\n"
-                       "3. Quit\n"
-                       "\n"
-                       "Enter your choice: ")
+    def start_game(self):
+        self.print_header()
+        user_input = ""
+        self.get_input()
+        if user_input == "1":
+            yyy = WorldGenerator()
+            rastikov_svet = yyy.generate_world()
+            self.world = rastikov_svet
+            yyy.save_world(rastikov_svet)
+        elif user_input == "2":
+            pass
+        elif user_input == "3":
+            print("\nClosing game.")
+            quit()
 
-    while user_input not in ["1", "2", "3"]:
-        print("Wrong input!")
-        return start_game()
-    else:
-        if int(user_input) == 1:
-            pass
-        elif int(user_input) == 2:
-            pass
-        elif int(user_input) == 3:
-            pass
+    def get_input(self):
+        user_input = input("1. Generate new world\n"
+                           "2. Load world\n"
+                           "3. Quit\n"
+                           "\n"
+                           "Enter your choice: ")
+
+        while user_input not in ["1", "2", "3"]:
+            print("\nWrong input! Insert 1, 2, or 3\n")
+            return self.get_input()
+        else:
+            return user_input
 
 
 rasto = RunGame()
-start_game()
+rasto.start_game()
