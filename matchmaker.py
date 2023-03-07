c8811ca9b9019a25e76dac000282cd3327defdfd
Gether1996 <pat.kredatus@gmail.com>
2023-03-07 20:51:20 +0100

    reduced code in  func assemble_teams_for_1v1

--- matchmaker.py
+++ matchmaker.py
@@ -40,26 +40,25 @@ class Matchmaker:
         sorted_gladiators_by_total_matches = self.m_gladiators
         sorted_gladiators_by_total_matches.sort(key=lambda x: x.m_GladiatorStatistics.m_total_matches, reverse=True)
         team_pairs_to_return = []
+        color_pool = list(range(1, config_NumOfColors + 1))
 
         while len(sorted_gladiators_by_total_matches) != 0:
             gladiator1 = sorted_gladiators_by_total_matches[0]
-            random_numbers_for_color = [x for x in range(1, (config_NumOfColors + 1))]
-            randomly_chosen_color = random.choice(random_numbers_for_color)
-            team1 = Team([gladiator1], randomly_chosen_color)
+            team1 = Team([gladiator1], random.choice(color_pool))
             team1.generate_name_for_team()
-            random_numbers_for_color.remove(randomly_chosen_color)
+            color_pool.remove(team1.m_team_color.value)
 
             if len(sorted_gladiators_by_total_matches) > 2:
                 gladiator2 = sorted_gladiators_by_total_matches[random.randint(1, 2)]
-                randomly_chosen_second_color = random.choice(random_numbers_for_color)
-                team2 = Team([gladiator2], randomly_chosen_second_color)
+                team2 = Team([gladiator2], random.choice(color_pool))
                 team2.generate_name_for_team()
+                color_pool = list(range(1, config_NumOfColors + 1))
 
             elif len(sorted_gladiators_by_total_matches) == 2:
                 gladiator2 = sorted_gladiators_by_total_matches[1]
-                randomly_chosen_second_color = random.choice(random_numbers_for_color)
-                team2 = Team([gladiator2], randomly_chosen_second_color)
+                team2 = Team([gladiator2], random.choice(color_pool))
                 team2.generate_name_for_team()
+                color_pool = list(range(1, config_NumOfColors + 1))
 
             else:
                 break
